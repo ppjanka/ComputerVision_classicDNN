9edecc9581b3aa9ced7efbc8ee2bd29804e7f29d
Patryk Pjanka <ppjanka@princeton.edu>
2019-03-01 16:17:52 -0500

    Labels now encoded as one-hot.

--- cv_io.py
+++ cv_io.py
@@ -29,36 +29,50 @@ def get_filenames (label, ext, preprocessed=False):
 
 # returns a handle to iterator.get_next() of images Dataset to be connected to a comp graph
 def image_batch_handle (preprocessed_folder):
-    import os
-    datasets = []
-    classes = list(os.walk(preprocessed_folder))[1:]
-    ntotal = 0
-    nclass = {}
-    for curr_class in classes:
-        label = curr_class[0].split('/')[-1]
-        img_filenames = preprocessed_folder + '/' + label + '/' + tf.constant(curr_class[2])
 
-        nclass[label] = len(curr_class[2])
-        labels = tf.constant(nclass[label] * [label,])
+    with tf.name_scope('IMG_INPUT'):
 
-        # create a dataset
-        datasets.append(tf.data.Dataset.from_tensor_slices((img_filenames, labels)))
-    ntotal = sum(nclass.values())
+        import os
+        datasets = []
+        classes = list(os.walk(preprocessed_folder))[1:]
+        ntotal = 0
+        nclass = {}
+        class2label = {}; label2class = {}; classno = 0
+        for curr_class in classes:
+            label = curr_class[0].split('/')[-1]
+            img_filenames = preprocessed_folder + '/' + label + '/' + tf.constant(curr_class[2])
 
-    # merge all datasets
-    final_dataset = datasets[0]
-    for dataset in datasets[1:]:
-        final_dataset = final_dataset.concatenate(dataset)
+            # save class identification
+            nclass[label] = len(curr_class[2])
+            nclass[classno] = nclass[label]
+            class2label[label] = classno
+            label2class[classno] = label
 
-    # shuffle and return iterator
-    final_dataset = final_dataset.shuffle(ntotal)
-    final_dataset = final_dataset.batch(batch_size)
-    iterator = final_dataset.make_one_shot_iterator()
-    iter_next = iterator.get_next()
+            # save class numbers as labels
+            labels = tf.constant(nclass[label] * [classno,])
 
-    # import images after drawing a batch
-    img_filenames, labels = iter_next
-    imgs = tf.map_fn(lambda img : tf.image.decode_jpeg(tf.read_file(img)), img_filenames, dtype=tf.uint8)
-    imgs = tf.image.convert_image_dtype(imgs, tf.float32)
+            # create a dataset
+            datasets.append(tf.data.Dataset.from_tensor_slices((img_filenames, labels)))
 
-    return imgs, labels, nclass
+            classno += 1
+
+        ntotal = sum(nclass.values())
+
+        # merge all datasets
+        final_dataset = datasets[0]
+        for dataset in datasets[1:]:
+            final_dataset = final_dataset.concatenate(dataset)
+
+        # shuffle and return iterator
+        final_dataset = final_dataset.shuffle(ntotal)
+        final_dataset = final_dataset.batch(batch_size)
+        iterator = final_dataset.make_one_shot_iterator()
+        iter_next = iterator.get_next()
+
+        # import images after drawing a batch
+        img_filenames, labels = iter_next
+        imgs = tf.map_fn(lambda img : tf.image.decode_jpeg(tf.read_file(img)), img_filenames, dtype=tf.uint8)
+        imgs = tf.image.convert_image_dtype(imgs, tf.float32)
+        labels = tf.one_hot(labels, classno)
+
+        return imgs, labels, nclass, class2label, label2class
