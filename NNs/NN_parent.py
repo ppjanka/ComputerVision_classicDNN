a696fd3d581b0a03f5028560f18a36b7781985ea
Patryk Pjanka <ppjanka@princeton.edu>
2019-04-18 19:53:05 -0400

    Plotting of tensorboard-like statistics for jupyter noytebooks partially implemented.

--- NNs/NN_parent.py
+++ NNs/NN_parent.py
@@ -39,7 +39,7 @@ class NN:
             result = graph.eval({self.X:Xin, self.keep_prob:1.})
         return result
 
-    def train (self, preprocessed_folder='./data_preprocessed', validation_size=0.2, n_epoch=1, input_model='last', output_model=None):
+    def train (self, preprocessed_folder='./data_preprocessed', validation_size=0.2, n_epoch=1, input_model='last', output_model=None, logfile=None):
 
         # initialize
         tf.logging.set_verbosity(tf.logging.INFO)
@@ -66,6 +66,14 @@ class NN:
             sess.run(tf.local_variables_initializer())
             print('done.', flush=True)
 
+            print('Initializing logfile.. ', end='', flush=True)
+            if logfile == None:
+                logfile = './workspace/%s.log' % (self.name,)
+            if not os.path.isfile(logfile):
+                with open(logfile, 'a') as f:
+                    f.write('Epoch\tAvgTrainLoss\tAvgTrainAcc\tAvgValLoss\tAvgValAcc\n')
+            print('done.', flush=True)
+
             # restore model if specified
             if input_model == 'last' and self.last_model != None:
                 print('Restoring the last available model:\n  %s' % self.last_model, flush=True)
@@ -80,6 +88,7 @@ class NN:
             merge = tf.summary.merge_all()
               
             # train
+            n_summary = 0
             for epoch in range(n_epoch):
                 sess.run(last_epoch.assign(tf.add(last_epoch, 1)))
 
@@ -101,12 +110,13 @@ class NN:
                         self.y = data['val']['y']
                         _val_cost, _val_acc = sess.run([val_cost, val_acc])
                         train_summary = sess.run(merge)
-                        writer.add_summary(train_summary)
+                        writer.add_summary(train_summary, n_summary)
                         writer.flush()
                         train_cost_avg.append(_train_cost)
                         train_acc_avg.append(_train_acc)
                         val_cost_avg.append(_val_cost)
                         val_acc_avg.append(_val_acc)
+                        n_summary += 1
                 except tf.errors.OutOfRangeError:
                     pass
 
@@ -119,3 +129,5 @@ class NN:
                 _ = saver.save(sess, self.last_model)
 
                 print('Epoch %i / %i completed:\n  avg. training loss: %.5e, avg. training accuracy: %.5f\n  avg. test loss: %.5e, avg. test accuracy: %.5f\n' % (sess.run(last_epoch)+1, sess.run(last_epoch+n_epoch-epoch), train_cost_avg, train_acc_avg, val_cost_avg, val_acc_avg), flush=True)
+                with open(logfile, 'a') as f:
+                    f.write('%i\t%.10e\t%.10e\t%.10e\t%.10e\n' % (sess.run(last_epoch)+1, train_cost_avg, train_acc_avg, val_cost_avg, val_acc_avg))
