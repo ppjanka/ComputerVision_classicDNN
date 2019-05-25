61a8d24474b26fc3c9f203ea0f1757cf2ada7505
Patryk Pjanka <ppjanka@princeton.edu>
2019-05-25 12:56:09 +0200

    Work in progress..

--- cv.py
+++ cv.py
@@ -7,4 +7,4 @@ import tensorflow as tf
 import AlexNet
 
 nn = AlexNet.AlexNet(n_classes=2, downscale=16, name='AlexNet_zero')
-nn.train(n_epoch=50, train_feed_dict={nn.dropout_keepProb:0.5}, val_feed_dict={nn.dropout_keepProb:1.0})
+nn.train(n_epoch=1, train_feed_dict={nn.dropout_keepProb:0.5}, val_feed_dict={nn.dropout_keepProb:1.0}, optimizer=tf.train.GradientDescentOptimizer, validation_size=0.)
