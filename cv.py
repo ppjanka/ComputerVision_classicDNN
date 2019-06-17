c5cd7045078c658ba2e96dee1e4fca5f262b6409
Patryk Pjanka <ppjanka@princeton.edu>
2019-06-17 10:23:31 +0200

    cv.py now ready for full network on Azure.

--- cv.py
+++ cv.py
@@ -27,8 +27,12 @@ import VGGNet
 #nn.train(n_epoch=50, train_feed_dict={nn.dropout_keepProb:1.0}, val_feed_dict={nn.dropout_keepProb:1.0}, validation_size=0.1, balance_sample=True, augment=False, pretrain=True, optimizer=tf.train.MomentumOptimizer, optimizer_kwargs={'learning_rate':0.01, 'momentum':0.9})
 
 # 5_augmented.log
-nn = VGGNet.VGGNet(n_classes=2, downscale=4, name='VGGNet_zero')
-nn.train(n_epoch=500, train_feed_dict={nn.dropout_keepProb:1.0}, val_feed_dict={nn.dropout_keepProb:1.0}, validation_size=0.1, balance_sample=True, augment=True, pretrain=True, optimizer=tf.train.MomentumOptimizer, optimizer_kwargs={'learning_rate':0.01, 'momentum':0.9})
+#nn = VGGNet.VGGNet(n_classes=2, downscale=4, name='VGGNet_zero')
+#nn.train(n_epoch=500, train_feed_dict={nn.dropout_keepProb:1.0}, val_feed_dict={nn.dropout_keepProb:1.0}, validation_size=0.1, balance_sample=True, augment=True, pretrain=True, optimizer=tf.train.MomentumOptimizer, optimizer_kwargs={'learning_rate':0.01, 'momentum':0.9})
+
+# 6_downscale1.log
+nn = VGGNet.VGGNet(n_classes=2, downscale=1, name='VGGNet_zero')
+nn.train(n_epoch=500, train_feed_dict={nn.dropout_keepProb:1.0}, val_feed_dict={nn.dropout_keepProb:1.0}, validation_size=0.1, balance_sample=True, augment=False, pretrain=True, pretrain_interval=500, optimizer=tf.train.MomentumOptimizer, optimizer_kwargs={'learning_rate':0.01, 'momentum':0.9})
 
 
 
